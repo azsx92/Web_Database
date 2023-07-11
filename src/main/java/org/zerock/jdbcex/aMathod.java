ed8f6096d301482cb8be9028f7d9af4b9d2dfa8e
azsx92 <azsx92@gmail.com>
2023-07-11 17:18:08 +0900

    raonseq

---
+++ src/main/java/org/zerock/jdbcex/aMathod.java
@@ -0,0 +1,13 @@
+package org.zerock.jdbcex;
+
+import com.sun.org.apache.bcel.internal.generic.ATHROW;
+
+public class aMathod {
+    public aMathod() {
+//        try {
+//            throw new Exception();
+//        } finally {
+//            System.out.println("finallyß");
+//        }
+    }
+}
---
+++ src/main/java/org/zerock/jdbcex/main.java
@@ -0,0 +1,33 @@
+package org.zerock.jdbcex;
+
+public class main {
+    public static void main(String[] args) {
+        //문제 1
+//        try {
+//             aMathod();
+//        } catch (Exception e) {
+//            System.out.println("Exception");
+//        }
+
+        // error 난다.
+
+
+//        문제 2
+//        float f = float("12");
+//        switch (f){
+//            case 12 :
+//                System.out.println("12");
+//            case 0 :
+//                System.out.println("12");
+//            default:
+//                System.out.println("10");
+//        }
+
+//        sql 문제
+//                사원 (사번 , 이름 , 부서)
+//                가족 ( 이름, 부서사번,사번)
+
+
+
+    }
+}
